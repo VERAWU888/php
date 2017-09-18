0d9bb8179451b2e0803fae1198d99925f0eccf21
vera <mubsd47868@gmail.com>
2017-09-18 22:54:52 +0800

    change animate.css and create index_32.html, js

---
+++ JQ/js/index_32.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $('.open').click(function(event) {
+    /* Act on the event */
+    $('.box').addClass('animated shake');
+  });
+
+});
+
+
+/*shake可以改成bounceInLeft, rubberBand, etc..
+
+以下是範例:
+
+$('.box').addClass('animated shake');
+ */
