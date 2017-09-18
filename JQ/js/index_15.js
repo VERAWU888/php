e7ff38ef099d75a44165b55e4d904616b27ab0f3
vera <mubsd47868@gmail.com>
2017-09-18 12:37:25 +0800

    change index_14 and add index_15

---
+++ JQ/js/index_15.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	//this is event1
+	$('.button1').click(function(event) {
+		$('.title1').hide();
+		
+	});
+	//this is event2
+	$('.button2').click(function(event) {
+		$('.title2').hide();
+
+
+	});
+});
+
+	//單行註解
+
