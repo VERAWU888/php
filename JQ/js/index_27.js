1dd816b8c75e5a76344e1438084086626e0b57a8
vera <mubsd47868@gmail.com>
2017-09-18 19:23:12 +0800

    add text in index_27.js

--- JQ/js/index_27.js
+++ JQ/js/index_27.js
@@ -9,3 +9,12 @@ $(document).ready(function() {
 
 /*show()的括號一定要寫上秒數(寫0也ok)，不然比box2早出現
 fadeIn 和 slideDown 不需要寫秒數，但要寫也沒關係*/
+
+/*$(document).ready(function() {
+	$('.open').click(function(event) {
+		$('.box1').delay(0).slideDown();
+		$('.box2').delay(1000).slideDown();
+		$('.box3').delay(2000).slideDown();
+	});
+});
+不管是show, fadeIn, slideDown都是可以的哦*/
