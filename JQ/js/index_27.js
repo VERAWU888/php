5c4e857c3efb15ce728a9186288aa9b5fb6bfc28
vera <mubsd47868@gmail.com>
2017-09-18 19:18:15 +0800

    create index_27.css, html and js

---
+++ JQ/js/index_27.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.open').click(function(event) {
+		/* Act on the event */
+		$('.box1').delay(0).fadeIn();
+		$('.box2').delay(1000).slideDown();
+		$('.box3').delay(2000).show(0);
+	});
+});
+
+/*show()的括號一定要寫上秒數(寫0也ok)，不然比box2早出現
+fadeIn 和 slideDown 不需要寫秒數，但要寫也沒關係*/
