9a5f3e4870b4e24df985089345d352b031b3872c
vera <mubsd47868@gmail.com>
2017-09-18 13:40:26 +0800

    modeified index_17.js

--- JQ/js/index_17.js
+++ JQ/js/index_17.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	//this is event1
+	//slideDown 出現
 	$('.button1').click(function(event) {
 		$('.text1').slideDown(1000);
 		
 	});
 	
+	//slideToggle 上下滑動
 	$('.button2').click(function(event) {
 		$('.text2').slideToggle(1000);
 	});
 
+	//slideUp 關閉
 	$('.button3').click(function(event) {
 		$('.text3').slideUp(1000);
 	});
