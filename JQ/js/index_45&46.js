3b0856893ed5a9f61472f57ddc55d9f402394614
vera <mubsd47868@gmail.com>
2017-09-19 19:11:44 +0800

    add font-awesome,imgs and create index_45&46.css, html,js

---
+++ JQ/js/index_45&46.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('.top a').click(function(event) {
+		/* Act on the event */
+		event.preventDefault();
+		$('html,body').animate({
+			scrollTop: 0
+		}, 3000);
+	});
+});
+
+
+$(document).ready(function() {
+	$("a[href$='.doc']").addClass('fa fa-file-word-o');
+	$("a[href$='.jpg']").addClass('fa fa-file-photo-o');
+	$("a[href$='.zip']").addClass('fa fa-file-zip-o');
+});
