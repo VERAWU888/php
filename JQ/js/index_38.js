4f0299db979c87f09eafa6bf6c772b93df14b1dd
vera <mubsd47868@gmail.com>
2017-09-19 12:38:35 +0800

    modified index_38.js

--- JQ/js/index_38.js
+++ JQ/js/index_38.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 	$('.cart > li > a').click(function(event) {
 		/* Act on the event */
 		event.preventDefault();
+		//選到的ul，以外的ul都隱藏起來
 		$(this).parent().siblings().find('ul').slideUp();
+		//選到的ul做出折疊效果
 		$(this).parent().find('ul').slideToggle();
 
 	});
