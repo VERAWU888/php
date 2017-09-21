848c0a4e8ea3226c0cca145309f46e6752ff23ec
vera <mubsd47868@gmail.com>
2017-09-21 11:50:56 +0800

    modified index-1.js and index-2.html

--- JS/JScript/index-1.js
+++ JS/JScript/index-1.js
@@ -4,3 +4,7 @@ alert('Holle Nice to see you !!');
 
 //index-2.html的ID(),textContent
 document.getElementById('title1').textContent = '修改後的標題';
+
+//ch3-13變數 number 介紹，加減乘除
+var cookiePrice = 35;
+document.getElementById('price').textContent = cookiePrice;
