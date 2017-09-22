81d8b9ada29a01f2cd8d760c42733d6cd1043615
vera <mubsd47868@gmail.com>
2017-09-22 12:32:21 +0800

    modified index-1.js and index-2.html, add css and img

--- JS/JScript/index-1.js
+++ JS/JScript/index-1.js
@@ -5,6 +5,26 @@ alert('Holle Nice to see you !!');
 //index-2.html的ID(),textContent
 document.getElementById('title1').textContent = '修改後的標題';
 
-//ch3-13變數 number 介紹，加減乘除
+//ch3-13:變數 number 介紹，加減乘除
 var cookiePrice = 35;
 document.getElementById('price').textContent = cookiePrice;
+
+//ch3-14:變數 string 介紹
+var polite = "Holle ";
+var indexName = "Chris ";
+var totalPolite = polite + indexName + "你好";
+document.getElementById('myName').textContent = totalPolite;
+
+//ch13-15:如何輸出內容
+var score = 90
+console.log(score);
+
+//ch3-17:變數-數字新增刪除修改
+var cookiePrice = 35;
+var myMoney = 15;
+var total = cookiePrice - myMoney;
+var five = cookiePrice * 5;
+document.getElementById('cookiePriceId').textContent = cookiePrice;
+document.getElementById('myMoneyId').textContent = myMoney;
+document.getElementById('totalId').textContent = total;
+document.getElementById('five').textContent = five;
