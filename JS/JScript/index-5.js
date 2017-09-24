78090bc41def5fbe081217c12df8da957c48cefa
vera <mubsd47868@gmail.com>
2017-09-24 22:59:54 +0800

    create index-5.js, html

---
+++ JS/JScript/index-5.js
@@ -0,0 +1,22 @@
+//ch5-34:陣列array 寫法
+var cornField = [];
+cornField.push(5);
+cornField.push(8);
+cornField.push(6);
+
+cornField [0] = 10;
+
+cornField[3] = 100;
+
+console.log('我總共有'+cornField.length+'個玉米田');
+
+console.log(cornField);
+
+//ch5-36:物件object 寫法
+var farm = {
+	farmer: '斯伯',
+};
+farm.chick = 15;
+
+console.log(farm);
+console.log(farm.chick);
