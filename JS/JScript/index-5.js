83914de69429f66a0ef89eda77e5c5b93db5d802
vera <mubsd47868@gmail.com>
2017-09-25 11:16:24 +0800

    modified index-5.js

--- JS/JScript/index-5.js
+++ JS/JScript/index-5.js
@@ -12,11 +12,64 @@ console.log('我總共有'+cornField.length+'個玉米田');
 
 console.log(cornField);
 
-//ch5-36:物件object 寫法
+//ch5-36:物件object 寫法, 37:物件+function的運用
 var farm = {
 	farmer: '斯伯',
+	duck:3,
+	dog:['亨利', '琪琪'],
+	goDinner: function(){
+		console.log(farm.farmer+',該回家吃晚餐');
+	},
+	poultrytotal: function(){
+		var num = farm.chick + farm.duck;
+		console.log('我家的農場共有'+num+'隻家禽');
+	}
 };
-farm.chick = 15;
+farm.chick = 15;//加在外面需要前面加上farm，也可以加在var farm = {}
 
-console.log(farm);
+farm.goDinner();
+farm.poultrytotal();
+
+console.log(farm.farmer);
 console.log(farm.chick);
+console.log('我的農場裡的第一隻狗叫'+farm.dog[0]);
+
+//家裡的人
+var house = {
+	room: 3,
+	father: 'Ed', 
+	mother: 'Reene',
+	son:['Luker','Lucas']
+};
+console.log(house.son[0]);
+
+//ch5-38:物件+陣列設計流程
+var sweetyshop = [
+	{
+		maker: '莉雅',
+		cookies: 7,
+		cake: 8,
+		pie: 4,
+		roll: 5,
+		bread: 6
+	},
+	{
+		maker: ['曼達琳','妮娜'],
+		cookies: 8,
+		cake: 7,
+		pie: 5,
+		roll: 4,
+		bread: 5
+	}
+];
+
+console.log(sweetyshop[1].maker[0])
+
+
+
+
+
+
+
+
+
