0c2fa2f3272998ff239d9d0649d2b2c9cf2d6f9b
vera <mubsd47868@gmail.com>
2017-09-26 15:00:04 +0800

    add ch7-62 coding

--- JS/JScript/index-7.js
+++ JS/JScript/index-7.js
@@ -54,7 +54,55 @@ for(var i =0;i<farmsTotal;i++){
     }
 }
 
+// ch7-60:for-i++寫法
+i = i+1
+i+= 1
+i++
+// 以上都在console裡練習的
 
+//ch7-61: for加總
+var farms = [
+{
+	farmer: '克特',
+	field: 6,
+	chick: 200,
+	banana: 5000
+},
+{
+	farmer: '理斯特',
+	field: 10,
+    chick: 50,
+    banana: 1000
+},
+{
+	farmer: '約翰',
+    field: 6,
+    chick: 120,
+    banana: 3215
+}
+]
+// 計算今年的香蕉採收總數
+var farmsTotal = farms.length;
+var bananaTotal = 0;
+
+for(var i =0;i<farmsTotal;i++){
+	bananaTotal+= farms[i].banana;
+}
+console.log('今年村子的香蕉採收量:'+bananaTotal);
+
+//ch7-62: for-break的運用
+
+//我要找一個農場，買50隻小雞
+var farmsTotal = farms.length;
+for(var i =0;i<farmsTotal;i++){
+	if(farms[i].chick>50){
+		console.log(farms[i].farmer+'的小雞剛好夠');
+		farms[i].chick -= 50;
+		console.log(farms[i].farmer+'的小雞剩下'+farms[i].chick);
+		break;
+	}
+}
+console.log(farms[0].chick);
 
 
 
