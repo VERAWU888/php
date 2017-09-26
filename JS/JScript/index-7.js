85476e4791adb83252b479b4848acc18d5c83740
vera <mubsd47868@gmail.com>
2017-09-26 11:11:53 +0800

    add for-if寫法in index-7.js

--- JS/JScript/index-7.js
+++ JS/JScript/index-7.js
@@ -9,3 +9,62 @@ for (var i=0;i<10;i++){
 	//0*0=0
 	//1*1=1
 }
+
+//ch7-58:for-array 寫法
+var farms = [
+{
+	farmer: '斯伯',
+	field: 6
+},
+{
+	farmer: '查理',
+	field: 10
+}
+]
+var farmsTotal = farms.length;
+for (var i=0;i<farmsTotal;i++){
+	console.log('第'+ (i+1) +'農場主人是'+farms[i].farmer)
+}
+
+//ch7-59: for-if 寫法
+var farms = [
+  {
+    farmer: '克特',
+    field: 6,
+    chick: 200
+  },
+  {
+    farmer: '理斯特',
+    field: 10,
+    chick: 50,
+  },
+  {
+    farmer: '約翰',
+    field: 6,
+    chick: 120
+  }
+]
+// 顯示哪些農場的小雞超過100隻以上
+
+var farmsTotal = farms.length;
+
+for(var i =0;i<farmsTotal;i++){
+    if(farms[i].chick>100){
+        console.log(farms[i].farmer+'的小雞超過100隻以上');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
