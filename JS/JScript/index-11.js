64fff35bb407219f8ac1bd602ca21bd871780276
vera <mubsd47868@gmail.com>
2017-09-29 14:04:51 +0800

    change and add codes in inedex-10final.css and index-11.js,css,html

--- JS/JScript/index-11.js
+++ JS/JScript/index-11.js
@@ -4,3 +4,22 @@
 document.getElementById('next').onclick = function(){
 	window.history.forward();
 }
+
+//ch11-111:透過JS設計列印功能
+document.getElementById('print').onclick = function(){
+	window.print();
+}
+document.getElementById('locat').onclick = function(){
+	console.log(location);
+}
+document.getElementById('open').onclick = function(){
+	window.open('http://www.google.com.tw');
+}
+
+//ch11-112:動態擷取瀏覽器高度 innerHeight
+document.querySelector('.hero').style.height = window.innerHeight+"px"
+
+//onresize可以讓背景隨著瀏覽器做伸縮
+window.onresize = function(){
+   document.querySelector('.hero').style.height = window.innerHeight+"px"
+}
