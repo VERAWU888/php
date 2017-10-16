a5f00b33186a0488dc65f6f59e60e6148c3d6b3d
Chris <gitcoffee550@gmail.com>
2017-10-16 21:51:08 +0800

    front_end_install_smarty

---
+++ Parity_front_end/smarty/hello.php
@@ -0,0 +1,9 @@
+<?php
+require "./libs/Smarty.class.php";
+
+$tpl = new Smarty;
+$tpl->assign("title", "測試用的網頁標題");
+$tpl->assign("content", "測試用的網頁內容");
+// 上面兩行也可以用這行代替
+// $tpl->assign(array("title" => "測試用的網頁標題", "content" => "測試用的網頁內容"));
+$tpl->display('hello.html');
---
+++ Parity_front_end/smarty/templates_c/da0d8854f921c397aeb5209c312ea0143498372e_0.file.hello.html.php
@@ -0,0 +1,36 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-10-16 15:47:10
+  from "/Applications/MAMP/htdocs/Parity_front_end/smarty/templates/hello.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_59e4b85eceb560_92124312',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'da0d8854f921c397aeb5209c312ea0143498372e' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/Parity_front_end/smarty/templates/hello.html',
+      1 => 1508160538,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_59e4b85eceb560_92124312 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+</head>
+<body>
+<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+</body>
+</html><?php }
+}
