e2dc10ff33a0f7cbf7ed9a114d008e844051f890
vera <mubsd47868@gmail.com>
2017-10-17 16:21:02 +0800

    add index.html

---
+++ Parity_front_end/smarty/templates_c/19e6b3fe29888f09a2da20f2036d752d7dec8cf9_0.file.hello.html.php
@@ -0,0 +1,36 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-10-17 09:26:42
+  from "/Applications/MAMP/htdocs/projectPHP/php/Parity_front_end/smarty/templates/hello.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_59e5b0b2700891_53021393',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '19e6b3fe29888f09a2da20f2036d752d7dec8cf9' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/projectPHP/php/Parity_front_end/smarty/templates/hello.html',
+      1 => 1508207961,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_59e5b0b2700891_53021393 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+</head>
+<body>
+<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+</body>
+</html><?php }
+}
