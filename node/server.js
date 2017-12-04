c6096812fbd3590ba51257442ee636e1d738169c
Chris <gitcoffee550@gmail.com>
2017-12-04 23:30:19 +0800

    node indatall

--- node/server.js
+++ node/server.js
@@ -4,4 +4,5 @@ var server = http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type':'text/plain'});
 	response.write('Hello World');
 	response.end();
+
 }).listen(3000);
